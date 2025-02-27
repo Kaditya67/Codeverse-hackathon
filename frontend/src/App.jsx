f48027e97e0dd76ac63a5b60bb0ddbeca4ceba00
kaditya67 <322adityakumar0004@dbit.in>
2025-02-27 19:24:26 +0530

    Frontend and backend seprated and connected

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("/api/hello/")
+            .then(response => response.json())
+            .then(data => setData(data.message));
+    }, []);
+
+    return (
+        <div>
+            <h1>React + Vite + Django</h1>
+            <p>{data ? data : "Loading..."}</p>
+        </div>
+    );
+}
+
+export default App;
