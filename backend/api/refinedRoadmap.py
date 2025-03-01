e98ddf7657e2b0c63b4c7b19f5fcbf939f845076
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-03-01 13:18:09 +0530

    Minor changes

---
+++ backend/api/refinedRoadmap.py
@@ -0,0 +1,8 @@
+# from .models import Roadmap, Topic
+# import json
+
+# def refineRoadmap(request):
+
+#     data = Roadmap.objects.all()
+#     print(data)
+#     pass
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import sample_api
 from .chatbot import sample_api as chatbot_sample 
 from .roadmapView import roadmap
+from .refinedRoadmap import refineRoadmap 
 
 urlpatterns = [
     path("hello/", sample_api),
     path("chatbot/", chatbot_sample),
     path('initroadmap/', roadmap),
+    path('refinedrmp/', refineRoadmap),
 ]
