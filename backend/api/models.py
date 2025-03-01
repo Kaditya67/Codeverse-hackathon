9491c1fa07f5545c7c1b0bce698a83ba259d3649
kaditya67 <322adityakumar0004@dbit.in>
2025-03-01 12:08:06 +0530

    Models created Topic,Roadmap

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Roadmap(models.Model):
+    language = models.CharField(max_length=100)
+    roadmap_requirements = models.TextField()
+    
+    topics = models.ManyToManyField(Topic, related_name="roadmaps")
+    done_topics = models.ManyToManyField(Topic, related_name="completed_roadmaps", blank=True)
+    not_interested_topics = models.ManyToManyField(Topic, related_name="skipped_roadmaps", blank=True)
+
+    def __str__(self):
+        return f"{self.language} Roadmap"
